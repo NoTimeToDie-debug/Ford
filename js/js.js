773d22c30d44dbb2ef130eec44354d7c6dca71f7
NoTimeToDie <nguyencatdat@gmail.com>
2021-06-01 13:32:54 +0700

    final

---
+++ js/js.js
@@ -0,0 +1,22 @@
+
+
+
+// document.getElementById('btn-SUV').onclick =function(){
+//     var btn =document.getElementById('a-name');
+//     btn.style.borderBottom='1px solid #0276b3';
+// }
+
+document.getElementById('dropdown-menu').style.maxHeight = "min(calc(100vh - 121px),700px)";
+
+document.querySelector('.popover').onclick = function(){
+    var btn =document.getElementsByClassName('popover').style.display='none';
+}
+
+// var container = document.querySelector('#dropdown-menu');
+
+//     // Nếu click bên ngoài đối tượng container thì ẩn nó đi
+//     if (!container.is(e.target) && container.has(e.target).length === 0)
+//     {
+//         container.hide();
+//     }
+
